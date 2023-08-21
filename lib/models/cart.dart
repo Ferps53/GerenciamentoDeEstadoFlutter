d33fd4e2a02b0727ead850a795a0ddf9b9327e41
Ferps53 <felipe.brostolin53@gmail.com>
2023-08-21 11:06:08 -0300

    Criação de métodos e classes de carrinho de compra

---
+++ lib/models/cart.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:gerenciamento_de_estado/models/cart_item.dart';
+import 'package:gerenciamento_de_estado/models/product.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itensCount {
+    return _items.length;
+  }
+
+  double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (value) => CartItem(
+          id: value.id,
+          productId: value.productId,
+          name: value.name,
+          quantity: value.quantity + 1,
+          price: value.price,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        product.id,
+        () => CartItem(
+          id: Random().nextDouble().toString(),
+          productId: product.id,
+          name: product.title,
+          quantity: 1,
+          price: product.price,
+        ),
+      );
+      notifyListeners();
+    }
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear() {
+    _items = {};
+    notifyListeners();
+  }
+}
