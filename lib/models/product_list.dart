d7600317611de98e323f91a5ac80b73e6c230168
Ferps53 <felipe.brostolin53@gmail.com>
2023-08-20 23:40:08 -0300

    Criada forma de filtrar os itens favoritos

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -2,12 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:gerenciamento_de_estado/data/dummy_data.dart';
 import 'package:gerenciamento_de_estado/models/product.dart';
 
-class ProductList with ChangeNotifier{
+class ProductList with ChangeNotifier {
   List<Product> _items = dummyProducts;
+  bool _showFavoritesOnly = false;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavoritesOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    } else {
+      return [..._items];
+    }
+  }
 
-  void addProduct(Product product){
+  void showFavoritesOnly() {
+    _showFavoritesOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoritesOnly = false;
+    notifyListeners();
+  }
+
+  void addProduct(Product product) {
     _items.add(product);
     notifyListeners();
   }
--- lib/screen/products_overview_screen.dart
+++ lib/screen/products_overview_screen.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:gerenciamento_de_estado/models/product_list.dart';
+import 'package:provider/provider.dart';
 import '../widget/product_grid.dart';
 
+enum FilterOptions {
+  Favorite,
+  All,
+}
+
 class ProductOverViewScreen extends StatelessWidget {
-  
   const ProductOverViewScreen({super.key});
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
         centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            color: Colors.white,
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: const Text("Somente Favoritos"),
+                value: FilterOptions.Favorite,
+              ),
+              const PopupMenuItem(
+                child: const Text("Todos"),
+                value: FilterOptions.All,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorite) {
+                provider.showFavoritesOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          ),
+        ],
         title: const Text(
           "Minha Loja!",
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body:const ProductGrid(),
+      body: const ProductGrid(),
     );
   }
 }
-
