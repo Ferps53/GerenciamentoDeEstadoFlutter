9f85f42678e1e94da2b854cdb4aba8b2c67a00b0
Ferps53 <felipe.brostolin53@gmail.com>
2023-08-26 19:06:22 -0300

    Alt: Uso de AnimatedContainer

--- lib/widget/auth_form.dart
+++ lib/widget/auth_form.dart
@@ -130,7 +130,13 @@ class _AuthFormState extends State<AuthForm>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: AnimatedBuilder(
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        // height: _isLogin() ? 310 : 400,
+        height: _isLogin() ? 310 : 400,
+        width: deviceSize.width * 0.75,
+        padding: const EdgeInsets.all(16),
         child: Form(
           key: _formKey,
           child: Column(
@@ -209,13 +215,6 @@ class _AuthFormState extends State<AuthForm>
             ],
           ),
         ),
-        animation: _heightAnimation!,
-        builder: (context, childForm) => Container(
-            // height: _isLogin() ? 310 : 400,
-            height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
-            width: deviceSize.width * 0.75,
-            padding: const EdgeInsets.all(16),
-            child: childForm),
       ),
     );
   }
