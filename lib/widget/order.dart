867e12cb10c6152055754cbd8a5a1fb8c54ad1ae
Ferps53 <felipe.brostolin53@gmail.com>
2023-08-26 20:47:06 -0300

    Add: animação nos pedidos!

--- lib/widget/order.dart
+++ lib/widget/order.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbols.dart';
 import 'package:intl/intl.dart';
 
 import '../models/order.dart';
@@ -11,38 +12,73 @@ class OrderWidget extends StatefulWidget {
   State<OrderWidget> createState() => _OrderWidgetState();
 }
 
-class _OrderWidgetState extends State<OrderWidget> {
+class _OrderWidgetState extends State<OrderWidget>
+    with TickerProviderStateMixin {
+  @override
+  void initState() {
+    _turnAnimation = Tween(
+      begin: 0.0,
+      end: 0.5,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOutCirc,
+      ),
+    );
+    super.initState();
+  }
+
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(
+      milliseconds: 300,
+    ),
+  );
+  Animation<double>? _turnAnimation;
+
   bool _expanded = false;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
-            title: Text(
-              "R\$${widget.order.total.toStringAsFixed(2)}",
-            ),
-            subtitle: Text(
-              DateFormat("dd/MM/yyyy hh:mm").format(widget.order.date),
-            ),
-            trailing: IconButton(
-              icon: const Icon(Icons.expand_more),
-              onPressed: () {
-                setState(
-                  () {
-                    _expanded = !_expanded;
+    final itemsHeight = (widget.order.products.length * 25.0) + 15;
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: _expanded ? itemsHeight + 80 : 80,
+      child: Card(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text(
+                "R\$${widget.order.total.toStringAsFixed(2)}",
+              ),
+              subtitle: Text(
+                DateFormat("dd/MM/yyyy hh:mm").format(widget.order.date),
+              ),
+              trailing: RotationTransition(
+                turns: _turnAnimation!,
+                child: IconButton(
+                  icon: const Icon(Icons.expand_more),
+                  onPressed: () {
+                    setState(
+                      () {
+                        _expanded = !_expanded;
+                        if (_expanded) {
+                          _controller.forward();
+                        } else {
+                          _controller.reverse();
+                        }
+                      },
+                    );
                   },
-                );
-              },
+                ),
+              ),
             ),
-          ),
-          if (_expanded)
-            Container(
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              height: _expanded ? itemsHeight : 0,
               padding: const EdgeInsets.symmetric(
                 horizontal: 15,
                 vertical: 4,
               ),
-              height: (widget.order.products.length * 25.0) + 10,
               child: ListView(
                 children: widget.order.products.map((product) {
                   return Row(
@@ -65,7 +101,8 @@ class _OrderWidgetState extends State<OrderWidget> {
                 }).toList(),
               ),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
